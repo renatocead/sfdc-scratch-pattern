d8fd628e0eb467109b499bc760b114cb2ecbd4c4
Renato Ceadareanu <rceadareanu@salesforce.com>
2022-05-09 09:23:23 -0300

    Teste

--- scripts/support/generateDestructive.sh
+++ scripts/support/generateDestructive.sh
@@ -2,7 +2,7 @@
 
 mkdir ./output
 cd ./output
-echo 'Generating SHA first commit: ' $(git merge-base $1 develop) 
-sfdx sgd:source:delta --to $(git merge-base $1 develop) --from  HEAD  --output . --loglevel error -r ../../../
+echo 'Generating SHA first commit:' $(git merge-base $1 develop) 
+sfdx sgd:source:delta --to HEAD --from $(git merge-base $1 develop) --output . -d -r ../../../ --ignore-destructive ../../../devops/.ignoreDestructive
 rm -rf ./package
 #teste
